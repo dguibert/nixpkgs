9d6c20c293975a4ca7e68f6b66c96f9d454dff86
David Guibert <david.guibert@gmail.com>
2017-05-09 16:51:04 +0200

    hdf5: updateScript with the new website url

--- pkgs/tools/misc/hdf5/update.nix
+++ pkgs/tools/misc/hdf5/update.nix
@@ -4,12 +4,12 @@ writeScript "update-hdf5" ''
 PATH=${lib.makeBinPath [ common-updater-scripts coreutils curl gnugrep gnused xidel jq ]}
 
 set -x
-url=https://support.hdfgroup.org/HDF5/release/obtainsrc5110.html
+url=https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/
 
 latest_version=$(curl $url | \
 	xidel -q - --extract "//a/@href" | \
-	grep tar.bz2 | \
-	sed -e "s:.*hdf5-\(.*\).tar.bz2:\1:")
+	grep hdf5- | \
+	sed -e "s:.*hdf5-\(.*\)\/:\1:" | sort -n | tail -n 1)
 
 update-source-version hdf5 "$latest_version"
 set +x
