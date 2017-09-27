f448c90feaddbfe8aca5ab448ed10f4683ab9620
David Guibert <david.guibert@gmail.com>
2017-09-27 10:09:26 +0200

    all-firmware: don't evaluate rtl8723bs-firmware on kernel>=4.12
    On kernel newer than 4.12, without this patch, the evaluation on
    all-firmware.nix fails:
    [...]
    Package ‘rtl8723bs-4.12.14-2017-04-06’ in /home/dguibert/code/nixpkgs/pkgs/os-specific/linux/rtl8723bs/default.nix:34 is marked as broken, refusing to evaluate.

--- nixos/modules/hardware/all-firmware.nix
+++ nixos/modules/hardware/all-firmware.nix
@@ -37,8 +37,12 @@ in {
       hardware.firmware = with pkgs; [
         firmwareLinuxNonfree
         intel2200BGFirmware
-        rtl8723bs-firmware
         rtl8192su-firmware
+      ];
+    })
+    (mkIf ((cfg.enableAllFirmware || cfg.enableRedistributableFirmware) && (! (versionAtLeast config.boot.kernelPackages.kernel.version "4.12"))) {
+      hardware.firmware = with pkgs; [
+        rtl8723bs-firmware
       ];
     })
     (mkIf cfg.enableAllFirmware {
