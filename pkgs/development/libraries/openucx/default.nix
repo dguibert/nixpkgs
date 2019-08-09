b30906af0a5b6e21f66b5f58af468b0074c31490
David Guibert <david.guibert@gmail.com>
2019-08-09 17:22:11 +0200

    openucx: 1.4.0 -> 1.6.0
    configured as:
    [...]
    configure: =========================================================
    configure: UCX build configuration:
    configure:       Multi-thread:   Disabled
    configure:          MPI tests:   Enabled
    configure:      Devel headers:
    configure:        UCT modules:   < ib rdmacm cma >
    configure:       CUDA modules:   < >
    configure:       ROCM modules:   < >
    configure:         IB modules:   < >
    configure:        UCM modules:   < >
    configure:       Perf modules:   < >
    configure: =========================================================
    
    (cherry picked from commit be1d7916d87daccc9067b36dc0525d4b1c90ceb5)

--- pkgs/development/libraries/openucx/default.nix
+++ pkgs/development/libraries/openucx/default.nix
@@ -8,20 +8,45 @@
 , libiberty
 , zlib
 , doxygen, perl
+, git
+, pkgconfig
+, rdma-core
+, libfabric
 }:
 
 stdenv.mkDerivation {
-  name = "openucx-1.4.0";
+  name = "openucx-1.6.0";
 
   src = fetchFromGitHub {
     owner = "openucx";
     repo = "ucx";
-    rev = "v1.4.0";
-    sha256 = "0p126wd8xd7x1fwc78hqmaaykyxyn8kpxd69i549d5c9w2a75j8y";
+    rev = "v1.6.0";
+    sha256 = "sha256:1bvsgjp8mzxd5j47g8714jfrzvcwl2g621fjj8wmrvkyw3m11gkz";
+    leaveDotGit=true;
   };
-  buildInputs = [ autoconf automake libtool doxygen perl ];
-  nativeBuildInputs = [ numactl binutils libbfd libiberty zlib ];
+  buildInputs = [ autoconf automake libtool doxygen perl git pkgconfig ];
+  nativeBuildInputs = [ numactl binutils libbfd libiberty zlib rdma-core libfabric ];
   preConfigure = "./autogen.sh";
+
+  configureFlags = [
+    "--with-verbs=${libfabric}"  #(=DIR)      Build OpenFabrics support, adding DIR/include,
+                    ## DIR/lib, and DIR/lib64 to the search path for
+                    ## headers and libraries
+    "--with-rc"       ## Compile with IB Reliable Connection support
+    "--with-ud"       ## Compile with IB Unreliable Datagram support
+    "--with-dc"       ## Compile with IB Dynamic Connection support
+    "--with-mlx5-dv"  ## Compile with mlx5 Direct Verbs support. Direct Verbs
+                      ## (DV) support provides additional acceleration
+                      ## capabilities that are not available in a regular
+                      ## mode.
+    "--with-ib-hw-tm" # Compile with IB Tag Matching support
+    "--with-dm" #       Compile with Device Memory support
+    #"--with-cm" #       Compile with IB Connection Manager support ## checking for ib_cm_send_req in -libcm... no
+    "--with-rdmacm=${rdma-core}" #     Enable the use of RDMACM (default is guess).
+  ## --with-knem=(DIR)       Enable the use of KNEM (default is guess).
+  ## --with-xpmem=(DIR)      Enable the use of XPMEM (default is guess).
+  ];
+
   meta = with stdenv.lib; {
     description = "A communication library implementing a high-performance messaging layer for MPI, PGAS, and RPC frameworks.";
     homepage = http://www.openucx.org;
