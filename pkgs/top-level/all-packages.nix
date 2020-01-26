5d55b62775f8b8d52f690113767cc53a6a5c0d35
David Guibert <david.guibert@gmail.com>
2020-01-26 20:20:30 +0100

    Revert "blitz: remove package"
    This reverts commit fd7ae52d5383ee9666e31799d38691ced3d22363.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1230,6 +1230,10 @@ in
 
   bliss = callPackage ../applications/science/math/bliss { };
 
+  blitz = callPackage ../development/libraries/blitz {
+    boost = boost160;
+  };
+
   blobfuse = callPackage ../tools/filesystems/blobfuse { };
 
   blockdiag = with python3Packages; toPythonApplication blockdiag;
