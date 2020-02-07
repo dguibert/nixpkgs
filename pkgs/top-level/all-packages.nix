803337f00d7ae5173619d966168959e21b92530c
David Guibert <david.guibert@gmail.com>
2020-02-07 17:00:20 +0100

    isBroken: init from nix-helpers

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1333,6 +1333,8 @@ with pkgs;
 
   iconConvTools = callPackage ../build-support/icon-conv-tools { };
 
+  isBroken = callPackage ../build-support/is-broken {};
+
   validatePkgConfig = makeSetupHook
     { name = "validate-pkg-config"; propagatedBuildInputs = [ findutils pkg-config ]; }
     ../build-support/setup-hooks/validate-pkg-config.sh;
