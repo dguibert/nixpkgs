63e22b5cc4f12628867fba089af357c52925bbc1
David Guibert <david.guibert@gmail.com>
2020-04-18 15:47:36 +0200

    npupnp: init at 2.1.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22255,6 +22255,8 @@ with pkgs;
 
   libunistring = callPackage ../development/libraries/libunistring { };
 
+  libnpupnp = callPackage ../development/libraries/npupnp { };
+
   libupnp = callPackage ../development/libraries/pupnp { };
 
   libwhereami = callPackage ../development/libraries/libwhereami { };
