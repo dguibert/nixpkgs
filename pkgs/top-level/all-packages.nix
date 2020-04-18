c48ab6b3e3f06ddb3cbe091ca40cd5422ef47aaf
David Guibert <david.guibert@gmail.com>
2020-04-18 16:10:11 +0200

    libupnpp: init at 0.18.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21513,6 +21513,8 @@ with pkgs;
 
   libupnp = callPackage ../development/libraries/pupnp { };
 
+  libupnpp = callPackage ../development/libraries/libupnpp { };
+
   libwhereami = callPackage ../development/libraries/libwhereami { };
 
   giflib     = callPackage ../development/libraries/giflib { };
