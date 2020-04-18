28cc498fc397d4fa00445f4f69a3beb634adb569
David Guibert <david.guibert@gmail.com>
2020-04-18 17:10:30 +0200

    upmpdcli: init at 1.4.7

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17110,6 +17110,10 @@ in
 
   unicon-lang = callPackage ../development/interpreters/unicon-lang {};
 
+  upmpdcli = callPackage ../tools/networking/upmpdcli {
+    python = python3;
+  };
+
   tsocks = callPackage ../development/libraries/tsocks { };
 
   unixODBC = callPackage ../development/libraries/unixODBC { };
