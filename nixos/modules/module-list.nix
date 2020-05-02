0d54b581ef0a497c4873b6d7f936e94c974bab0c
David Guibert <david.guibert@gmail.com>
2020-05-02 12:29:05 +0200

    upmpdcli: add nixos service

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -318,6 +318,7 @@
   ./services/audio/spotifyd.nix
   ./services/audio/squeezelite.nix
   ./services/audio/tts.nix
+  ./services/audio/upmpdcli.nix
   ./services/audio/ympd.nix
   ./services/backup/automysqlbackup.nix
   ./services/backup/bacula.nix
