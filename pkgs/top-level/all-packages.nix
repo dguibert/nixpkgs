4ee0b750c6e27eb61b9fee5dce7462c258d23754
David Guibert <david.guibert@gmail.com>
2020-06-19 17:31:57 +0200

    sssd: ldap from python3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10445,7 +10445,7 @@ with pkgs;
 
   sssd = callPackage ../os-specific/linux/sssd {
     inherit (perlPackages) Po4a;
-    inherit (python27Packages) ldap;
+    inherit (python3Packages) ldap;
   };
 
   sentry-cli = callPackage ../development/tools/sentry-cli {
