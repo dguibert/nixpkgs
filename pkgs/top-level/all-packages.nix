10363910d63d0a9078501dc63efc138643301543
David Guibert <david.guibert@gmail.com>
2020-06-19 17:31:57 +0200

    sssd: ldap from python3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8086,7 +8086,7 @@ in
 
   sssd = callPackage ../os-specific/linux/sssd {
     inherit (perlPackages) Po4a;
-    inherit (python27Packages) ldap;
+    inherit (python3Packages) ldap;
   };
 
   vtun = callPackage ../tools/networking/vtun {
