60a1bd72b3a4f1686278f59fe6987a843ea41e70
David Guibert <david.guibert@gmail.com>
2020-06-29 14:56:47 +0200

    singularity-tools: buildImageWithNixDb

--- pkgs/build-support/singularity-tools/default.nix
+++ pkgs/build-support/singularity-tools/default.nix
@@ -11,7 +11,34 @@
 , util-linux
 , runtimeShell
 , e2fsprogs
+, nix
+, closureInfo
 }:
+
+# WARNING: this API is unstable and may be subject to backwards-incompatible changes in the future.
+let
+
+  mkDbExtraCommand = contents: let
+    contentsList = if builtins.isList contents then contents else [ contents ];
+  in ''
+    echo "Generating the nix database..."
+    echo "Warning: only the database of the deepest Nix layer is loaded."
+    echo "         If you want to use nix commands in the container, it would"
+    echo "         be better to only have one layer that contains a nix store."
+
+    export NIX_REMOTE=local?root=$PWD
+    # A user is required by nix
+    # https://github.com/NixOS/nix/blob/9348f9291e5d9e4ba3c4347ea1b235640f54fd79/src/libutil/util.cc#L478
+    export USER=nobody
+    ${nix}/bin/nix-store --load-db < ${closureInfo {rootPaths = contentsList;}}/registration
+
+    mkdir -p nix/var/nix/gcroots/docker/
+    for i in ${lib.concatStringsSep " " contentsList}; do
+    ln -s $i nix/var/nix/gcroots/docker/$(basename $i)
+    done;
+  '';
+
+in
 rec {
   shellScript = name: text:
     writeScript name ''
@@ -25,10 +52,11 @@ rec {
     , contents ? [ ]
       # May be "apptainer" instead of "singularity"
     , projectName ? (singularity.projectName or "singularity")
+    , extraCommands ? ""
     }:
     runCommand "${projectName}-layer-${name}"
       {
-        inherit contents;
+        inherit contents extraCommands;
       } ''
       mkdir $out
       for f in $contents ; do
@@ -47,6 +75,7 @@ rec {
     , runAsRoot ? null
     , memSize ? 512
     , singularity ? defaultSingularity
+    , extraCommands ? ""
     }:
     let
       projectName = singularity.projectName or "singularity";
@@ -99,6 +128,10 @@ rec {
               done
             done
 
+            if [[ -n $extraCommands ]]; then
+              (eval "$extraCommands")
+            fi
+
             # Create runScript and link shell
             if [ ! -e bin/sh ]; then
               ln -s ${runtimeShell} bin/sh
@@ -119,4 +152,15 @@ rec {
 
     in
     result;
+
+  # Build an image and populate its nix database with the provided
+  # contents. The main purpose is to be able to use nix commands in
+  # the container.
+  # Be careful since this doesn't work well with multilayer.
+  buildImageWithNixDb = args@{ contents ? null, extraCommands ? "", ... }: (
+    buildImage (args // {
+      extraCommands = (mkDbExtraCommand contents) + extraCommands;
+    })
+  );
+
 }
