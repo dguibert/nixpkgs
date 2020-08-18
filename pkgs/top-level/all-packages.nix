29f6e7c6cb1b7be544225dfc37575f8d4f6a828b
David Guibert <david.guibert@gmail.com>
2020-08-18 08:27:05 +0200

    fuse-migratefs: init at 0.6.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8027,6 +8027,8 @@ with pkgs;
 
   fuse-archive = callPackage ../tools/filesystems/fuse-archive { };
 
+  fuse-migratefs = callPackage ../tools/filesystems/fuse-migratefs { };
+
   fuse-overlayfs = callPackage ../tools/filesystems/fuse-overlayfs { };
 
   fusee-interfacee-tk = callPackage ../applications/misc/fusee-interfacee-tk { };
