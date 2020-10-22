a8dae164e34731f746628aa1f42bfffc158f1a65
David Guibert <david.guibert@gmail.com>
2020-10-22 09:38:02 +0200

    datalad: init at 0.13.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1860,6 +1860,8 @@ in {
 
   datadog = callPackage ../development/python-modules/datadog { };
 
+  datalad = callPackage ../development/python-modules/datalad { };
+
   datamodeldict = callPackage ../development/python-modules/datamodeldict { };
 
   datasets = callPackage ../development/python-modules/datasets { };
