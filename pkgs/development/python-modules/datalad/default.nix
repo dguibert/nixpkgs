4648c9eb3c8d1a4f54db231d1b9c0343ec9af94b
David Guibert <david.guibert@gmail.com>
2020-10-22 09:38:02 +0200

    datalad: init at 0.13.4

---
+++ pkgs/development/python-modules/datalad/default.nix
@@ -0,0 +1,60 @@
+{ PyGithub, annexremote, appdirs, boto, buildPythonPackage, chardet, fasteners
+, fetchPypi, humanize, iso8601, jsmin, keyring, keyrings-alt, lib, msgpack
+, patool, requests, simplejson, tqdm, whoosh, wrapt, setuptools
+, fetchFromGitHub
+}:
+
+buildPythonPackage rec {
+  pname = "datalad";
+  version = "0.13.4";
+
+  src = fetchFromGitHub {
+    owner = "datalad";
+    repo = "datalad";
+    rev = "refs/tags/0.13.4";
+    sha256 = "sha256-0dAdNA7fhX5bKaw/jSHpcU2KV6BdEIYY9XBEH4STPXA=";
+  };
+
+  propagatedBuildInputs = [
+    appdirs
+    chardet
+    iso8601
+    humanize
+    fasteners
+    patool
+    tqdm
+    wrapt
+    annexremote
+    boto
+    keyring
+    keyrings-alt
+    msgpack
+    requests
+    jsmin
+    PyGithub
+    simplejson
+    whoosh
+    setuptools
+  ];
+
+  # TODO FIXME
+  doCheck = false;
+
+  preConfigure = ''
+    # ERROR: Could not find a version that satisfies the requirement distro; python_version >= "3.8" (from datalad==0.13.4) (from versions: none)
+    # ERROR: No matching distribution found for distro; python_version >= "3.8" (from datalad==0.13.4)
+    sed -i -e "s@'distro; python_version@#'distro; python_version@" setup.py
+  '';
+
+  postFixup = ''
+    for f in $out/lib/python*/site-packages/datalad/resources/procedures/*.py; do
+      echo "wrapping $f"
+      patchPythonScript $f
+    done
+  '';
+
+
+  meta = with lib; {
+    description = "data distribution geared toward scientific datasets";
+  };
+}
