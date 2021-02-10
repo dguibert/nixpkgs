70db8e6d49666b8704da15f4e18c7fcce1c9e831
David Guibert <david.guibert@gmail.com>
2021-02-10 13:32:32 +0100

    linux_testing: 5.11-rc5 -> 5.11-rc7

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.11-rc5";
+  version = "5.11-rc7";
   extraMeta.branch = "5.11";
 
   # modDirVersion needs to be x.y.z, will always add .0
@@ -11,7 +11,7 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "029nps41nrym5qz9lq832cys4rai04ig5xp9ddvrpazzh0lfnr4q";
+    sha256 = "sha256-Ybk7n3JRI3+lWT61DRtYRXUsKGXMqyi9s4oY/a/PJyA=";
   };
 
   # Should the testing kernels ever be built on Hydra?
