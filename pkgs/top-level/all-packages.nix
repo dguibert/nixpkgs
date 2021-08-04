b545eb96e2581af86b8625698c63dbd91a7e12be
David Guibert <david.guibert@gmail.com>
2021-08-04 12:22:12 +0200

    nixos-option: nixStable

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32468,7 +32468,7 @@ with pkgs;
 
   nix-linter = haskell.lib.justStaticExecutables (haskellPackages.nix-linter);
 
-  nixos-option = callPackage ../tools/nix/nixos-option { };
+  nixos-option = callPackage ../tools/nix/nixos-option { nix = nixStable; };
 
   nix-pin = callPackage ../tools/package-management/nix-pin { };
 
