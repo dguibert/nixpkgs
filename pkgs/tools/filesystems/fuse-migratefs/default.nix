5b45104a72c196173bb70e164eb818d9236298c2
David Guibert <david.guibert@gmail.com>
2022-02-26 22:22:33 +0100

    fuse-migratefs: 0.6.1 -> 0.6.3

--- pkgs/tools/filesystems/fuse-migratefs/default.nix
+++ pkgs/tools/filesystems/fuse-migratefs/default.nix
@@ -2,13 +2,14 @@
 
 stdenv.mkDerivation rec {
   pname = "fuse-migratefs";
-  version = "0.6.1";
+  version = "0.6.3";
 
   src = fetchFromGitHub {
     owner = "stanford-rc";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-H2NYxInPTSidcXgrhC6pwkjyR7e0aaGF4LU+XXDshUE=";
+    #rev = "v${version}"; v0.6.3 not yet publish
+    rev = "97a1720a566a6f789248e60020217d03d35c5d22";
+    sha256 = "sha256-LtCBhqRsy0imGTh91qthhH76QBDOlqQSr7GXPrtE9vA=";
   };
 
   nativeBuildInputs = [ autoreconfHook pkg-config ];
