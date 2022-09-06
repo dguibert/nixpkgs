08fd3f28b01be76cf5bb4059be3cc2b607c609da
David Guibert <david.guibert@gmail.com>
2022-09-06 15:05:38 +0200

    requests-ftp: init 0.3.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10192,6 +10192,8 @@ self: super: with self; {
 
   requests-file = callPackage ../development/python-modules/requests-file { };
 
+  requests-ftp = callPackage ../development/python-modules/requests-ftp { };
+
   requests-futures = callPackage ../development/python-modules/requests-futures { };
 
   requests-http-signature = callPackage ../development/python-modules/requests-http-signature { };
