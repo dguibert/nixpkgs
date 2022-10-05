4b40f7a4b354b812915f64a8d79ad0b0dffb12e7
David Guibert <david.guibert@gmail.com>
2022-10-05 09:41:28 +0200

    cudatoolkit: 11.7.0 -> 11.7.1

--- pkgs/development/compilers/cudatoolkit/redist/extension.nix
+++ pkgs/development/compilers/cudatoolkit/redist/extension.nix
@@ -11,7 +11,7 @@ final: prev: let
     "11.4" = ./manifests/redistrib_11.4.4.json;
     "11.5" = ./manifests/redistrib_11.5.2.json;
     "11.6" = ./manifests/redistrib_11.6.2.json;
-    "11.7" = ./manifests/redistrib_11.7.0.json;
+    "11.7" = ./manifests/redistrib_11.7.1.json;
     "11.8" = ./manifests/redistrib_11.8.0.json;
     "12.0" = ./manifests/redistrib_12.0.1.json;
     "12.1" = ./manifests/redistrib_12.1.1.json;
