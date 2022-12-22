06cefb04990a2b0de1d4e0216536446332e1436a
David Guibert <david.guibert@gmail.com>
2022-12-22 23:50:36 +0100

    slvs: init 1.0.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10842,6 +10842,8 @@ self: super: with self; {
 
   slugid = callPackage ../development/python-modules/slugid { };
 
+  slvs = callPackage ../development/python-modules/slvs { };
+
   sly = callPackage ../development/python-modules/sly { };
 
   smart-meter-texas = callPackage ../development/python-modules/smart-meter-texas { };
