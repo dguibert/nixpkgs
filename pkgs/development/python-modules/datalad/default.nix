32cf7d961d9861afd9ef304ba32dcaed15d03b38
David Guibert <david.guibert@gmail.com>
2026-06-08 14:49:30 +0200

    datalad: set pytest root dir

--- pkgs/development/python-modules/datalad/default.nix
+++ pkgs/development/python-modules/datalad/default.nix
@@ -179,6 +179,7 @@ buildPythonPackage (finalAttrs: {
   pytestFlags = [
     # Deprecated in 3.13. Use exc_type_str instead.
     "-Wignore::DeprecationWarning"
+    "datalad/tests"
   ];
 
   # Tests use ports on localhost
