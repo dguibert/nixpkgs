a6d24a48897b4dea478a17741e691381a8d04105
David Guibert <david.guibert@gmail.com>
2026-06-21 17:14:53 +0200

    fix "stdenv: drop redundant crossOverlays defaults"
    Commit b9e52062740278903f9db96940a8a6803deaa4b2 "stdenv: drop
    redundant crossOverlays defaults" stated that only
    pkgs/top-level/default.nix and pkgs/stdenv/cross/default.nix require
    crossOverlays argument.
    
    But pkgs/stdenv/custom.nix includes pkgs/top-level/default.nix as
    well, needing crossOverlays.

--- pkgs/stdenv/custom/default.nix
+++ pkgs/stdenv/custom/default.nix
@@ -15,6 +15,7 @@ let
       localSystem
       crossSystem
       overlays
+      crossOverlays
       ;
     # Remove config.replaceStdenv to ensure termination.
     config = removeAttrs config [ "replaceStdenv" ];
